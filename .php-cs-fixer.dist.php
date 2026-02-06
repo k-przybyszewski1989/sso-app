16d3b2b1acca99956370b791e07ac4d9bca7bed6
Krzysztof Przybyszewski <krzysztof.przybyszewski@docplanner.com>
2026-02-06 19:42:49 +0100

    static analysis

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor')
+    ->exclude('node_modules')
+    ->notPath('src/Kernel.php')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+        '@DoctrineAnnotation' => true,
+        'declare_strict_types' => true,
+        'strict_param' => true,
+        'strict_comparison' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['imports_order' => ['class', 'function', 'const']],
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => true,
+        'phpdoc_separation' => false,
+        'phpdoc_types_order' => ['null_adjustment' => 'always_last'],
+        'return_type_declaration' => ['space_before' => 'none'],
+        'single_line_throw' => false,
+        'blank_line_after_opening_tag' => true,
+        'blank_line_before_statement' => [
+            'statements' => ['return', 'throw', 'try'],
+        ],
+        'cast_spaces' => ['space' => 'single'],
+        'concat_space' => ['spacing' => 'one'],
+        'method_argument_space' => [
+            'on_multiline' => 'ensure_fully_multiline',
+        ],
+        'modifier_keywords' => [
+            'elements' => ['property', 'method', 'const'],
+        ],
+        'nullable_type_declaration' => ['syntax' => 'question_mark'],
+        'nullable_type_declaration_for_default_null_value' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+;
